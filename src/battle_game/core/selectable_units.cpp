fa9d183cbd2fb27b042833495f1c9e15883084bf
yangzhen565 <13524753451@163.com>
2025-01-11 17:32:18 +0800

    Add files via upload

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -22,6 +22,8 @@ void GameCore::GeneratePrimaryUnitList() {
    * TODO: Add Your Unit Here!
    * */
   ADD_SELECTABLE_UNIT(unit::Tank);
+  ADD_SELECTABLE_UNIT(unit::YZTank);
+
 
   unit.reset();
 }
