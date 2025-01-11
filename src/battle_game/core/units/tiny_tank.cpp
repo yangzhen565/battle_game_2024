ac60ab4411afde00f4c288ff5d8bcb8807425bb7
yangzhen565 <13524753451@163.com>
2025-01-11 19:26:50 +0800

    debug

--- src/battle_game/core/units/tiny_tank.cpp
+++ src/battle_game/core/units/tiny_tank.cpp
@@ -146,7 +146,7 @@ void Tank::Fire() {
   }
   if (fire_count_down_) {
     fire_count_down_--;
-    if (fire_count_down == 55||fire_count_down==50) {
+    if (fire_count_down_ == 55||fire_count_down_==50) {
       auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
       GenerateBullet<bullet::CannonBall>(
           position_ + Rotate({0.0f, 1.2f}, turret_rotation_), turret_rotation_,
